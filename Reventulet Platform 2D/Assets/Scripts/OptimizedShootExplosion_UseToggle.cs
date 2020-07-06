92ff5ac16f36a615e63c2ed29250e3709d149d87
DavideWu <56259661+DavideWu@users.noreply.github.com>
2020-07-06 15:49:59 +0200

    new: FireBall.Skill , SprintVFX

---
+++ Reventulet Platform 2D/Assets/Scripts/OptimizedShootExplosion_UseToggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimizedShootExplosion_UseToggle : MonoBehaviour
+{
+    public GameObject BulletExplosion;
+    
+   
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        BulletExplosion.transform.position = transform.position;
+        BulletExplosion.SetActive(!BulletExplosion.activeInHierarchy);
+        
+
+    }
+}
---
+++ Reventulet Platform 2D/Assets/Scripts/ToggleOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOnCollision : MonoBehaviour
+{
+    public GameObject ObjectToToggle;
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ObjectToToggle.SetActive(!ObjectToToggle.activeInHierarchy);
+    }
+}
