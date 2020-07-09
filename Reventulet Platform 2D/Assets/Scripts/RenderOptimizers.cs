0370fa6df82ccc9fb31b3cf4b45d096d689cbe7d
DavideWu <56259661+DavideWu@users.noreply.github.com>
2020-07-09 12:42:49 +0200

    Scenes.TouchScreen. fixedBug, L5.Obstacles, minorBugsFixed

--- Reventulet Platform 2D/Assets/Scripts/RenderOptimizers.cs
+++ Reventulet Platform 2D/Assets/Scripts/RenderOptimizers.cs
@@ -6,39 +6,25 @@ public class RenderOptimizers : MonoBehaviour
 {
     public GameObject OBJ;
     private Transform target;
-    private Renderer render;
     [Range(15f, 30f)]
     public float RenderDist = 15f;
-    private bool togle = true;
     // Start is called before the first frame update
     void Start()
     {
         OBJ.SetActive(false);
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        render = OBJ.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) < RenderDist)
+        if ((Vector2.Distance(transform.position, target.position) > RenderDist))
         {
-            if(togle)
-            {
-                OBJ.SetActive(true);
-                togle = false;
-                
-            }
-                  
+            OBJ.SetActive(false);
         }
-        if (OBJ.gameObject.CompareTag("SceneOBJ") && (Vector2.Distance(transform.position, target.position) > RenderDist))
+        else if ((Vector2.Distance(transform.position, target.position) < RenderDist))
         {
-               render.enabled = false;
-               togle = true;
-        }
-        else if (OBJ.gameObject.CompareTag("SceneOBJ") && (Vector2.Distance(transform.position, target.position) < RenderDist))
-        {
-            render.enabled = true;
+            OBJ.SetActive(true);
         }
     }
 }
