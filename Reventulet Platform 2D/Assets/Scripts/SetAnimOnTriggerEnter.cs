fa3936800da809aa8d8f12aef97caa82465ed09f
DavideWu <56259661+DavideWu@users.noreply.github.com>
2020-07-14 06:13:30 +0200

    add: Lv selection/ start game. BG and buttons. UI

---
+++ Reventulet Platform 2D/Assets/Scripts/SetAnimOnTriggerEnter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAnimOnTriggerEnter : MonoBehaviour
+{
+    public Animator animationToChange;
+    public string NameParameterBool;
+
+    void Start()
+    {
+        animationToChange = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("light"))
+        {
+            animationToChange.SetBool(NameParameterBool, true);
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("light"))
+        {
+            animationToChange.SetBool(NameParameterBool, false);
+        }
+
+    }
+}
