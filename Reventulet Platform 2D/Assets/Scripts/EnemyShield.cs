f52ee77754d7d702a47418805824692560888098
DavideWu <56259661+DavideWu@users.noreply.github.com>
2020-07-18 13:35:59 +0200

    Add: tutorial, IA Enemy4-Boss4

---
+++ Reventulet Platform 2D/Assets/Scripts/EnemyShield.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShield : MonoBehaviour
+{
+    private SpriteRenderer render;
+    public GameObject AttR;
+    public GameObject AttL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (render.flipX == false)
+         {
+               AttR.SetActive(true);
+                AttL.SetActive(false);
+         }
+         else if (render.flipX == true)
+         {
+               AttL.SetActive(true);
+                AttR.SetActive(false);
+         }  
+    }
+
+    private void stun()
+    {
+        AttR.SetActive(false);
+        AttL.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        stun();
+    }
+}
